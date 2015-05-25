bcf853d15e91f6e5a616fb7c0120e7459a36ffb6
smorris383 <smorris383@gmail.com>
2015-05-25 01:22:33 -0600

    Merged with Fresh Project

---
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets._2D
+{
+	[RequireComponent(typeof(PlatformerCharacter2D))]
+	public class Platformer2DUserControl : MonoBehaviour {
+		private PlatformerCharacter2D m_Character;
+		private bool m_Jump;
+		private bool crouch;
+
+		private void Awake() {
+			m_Character = GetComponent<PlatformerCharacter2D>();
+		}
+
+		private void Update() {
+			m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+			crouch = Input.GetKey(KeyCode.LeftControl);
+			float h = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+			float v = CrossPlatformInputManager.GetAxisRaw("Vertical");
+
+			m_Character.Move(h, crouch, m_Jump, v);
+		}
+	}
+}
